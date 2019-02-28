3e6b2e4cfa175f40b7df4ae620906b3b9dd06d5e
teppeis <teppeis@gmail.com>
2019-02-28 09:28:47 +0900

    fix: remove unused deps.js from example/configs

--- examples/configs/deps.js
+++
@@ -1,7 +0,0 @@
-goog.addDependency('../../../../js/chunk1.js', ['app.chunk1'], ['goog.module.ModuleManager'], {'lang': 'es6'});
-goog.addDependency('../../../../js/chunk2.js', ['app.chunk2'], ['goog.module.ModuleManager'], {'lang': 'es6'});
-goog.addDependency('../../../../js/chunks.js', ['app.chunks'], ['goog.html.legacyconversions', 'goog.module.ModuleLoader', 'goog.module.ModuleManager'], {'lang': 'es6'});
-goog.addDependency('../../../../js/chunks_init.js', ['app.chunks_init'], ['goog.ui.Component'], {});
-goog.addDependency('../../../../js/page1.js', ['app.page1'], [], {});
-goog.addDependency('../../../../js/page2.js', ['app.page2'], [], {});
-
