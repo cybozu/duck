718d585f4a5427a7d3f630a837cb8d71eecf0ca1
teppeis <teppeis@gmail.com>
2019-05-25 23:51:55 +0900

    fix: increse timeout

--- test/bin-test/bin.js
+++ test/bin-test/bin.js
@@ -10,7 +10,8 @@ const bin = path.resolve(__dirname, '..', '..', 'bin', 'duck.js');
 
 // Run this test after tsc
 describe('bin/duck.js', () => {
-  it('is executable', async () => {
+  it('is executable', async function() {
+    this.timeout(5000);
     const {stderr} = await execFileP(bin, ['--help']);
     assert.equal(stderr, '');
   });
