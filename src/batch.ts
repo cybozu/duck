ef153856ae812f5690448987f2d05d79844a8f00
teppeis <teppeis@gmail.com>
2019-05-26 16:01:03 +0900

    fix: apply default webpack externals

--- src/batch.ts
+++ src/batch.ts
@@ -12,8 +12,41 @@ import {DuckConfig} from './duckconfig';
 export async function getFaastCompiler(
   config: DuckConfig
 ): Promise<FaastModuleProxy<typeof compilerFaastFunctions, CommonOptions, any>> {
+  const batchOptions = getBatchOptions(config);
   const m = process.env.AWS
-    ? await faastAws(compilerFaastFunctions, config.batchOptions as AwsOptions)
-    : await faastLocal(compilerFaastFunctions, config.batchOptions as LocalOptions);
+    ? await faastAws(compilerFaastFunctions, batchOptions as AwsOptions)
+    : await faastLocal(compilerFaastFunctions, batchOptions as LocalOptions);
   return m;
+}
+
+function getBatchOptions(config: DuckConfig): AwsOptions | LocalOptions {
+  const {batchOptions = {}} = config;
+  if (!batchOptions.webpackOptions) {
+    batchOptions.webpackOptions = {};
+  }
+  if (!batchOptions.webpackOptions.externals) {
+    batchOptions.webpackOptions.externals = [];
+  }
+  if (!Array.isArray(batchOptions.webpackOptions.externals)) {
+    batchOptions.webpackOptions.externals = [batchOptions.webpackOptions.externals];
+  }
+  batchOptions.webpackOptions.externals = [
+    ...batchOptions.webpackOptions.externals,
+    ...defaultWebpackExternals(),
+  ];
+  return batchOptions;
+}
+
+function defaultWebpackExternals(): import('webpack').ExternalsElement[] {
+  return [
+    /^aws-sdk\/?/,
+    'google-closure-compiler-js',
+    'google-closure-compiler-linux',
+    'google-closure-compiler-osx',
+    // used in google-closure-compiler/lib/(grunt|gulp)
+    'chalk',
+    // used in google-closure-compiler/lib/gulp
+    /^gulp($|-)/,
+    /^vinyl($|-)/,
+  ];
 }
