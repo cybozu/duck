7491c0f3a9307c22081deabcbff5eeaaa47519c0
teppeis <teppeis@gmail.com>
2019-06-11 23:18:04 +0900

    fix: await faastjs cleanup

--- src/commands/buildJs.ts
+++ src/commands/buildJs.ts
@@ -91,7 +91,7 @@ export async function buildJs(
     await waitAllAndThrowIfAnyCompilationsFailed(promises, entryConfigPaths);
   } finally {
     if (faastModule) {
-      faastModule.cleanup();
+      await faastModule.cleanup();
     }
   }
 
