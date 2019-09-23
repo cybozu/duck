2e2c0fc92738b92683afe30aa95df945188b745f
Toru Kobayashi <koba0004@gmail.com>
2019-09-23 20:48:33 +0900

    fix: follow symlinks in deps.js (#226)

--- src/gendeps.ts
+++ src/gendeps.ts
@@ -100,7 +100,7 @@ export async function getDependencies(
       if (entryConfig["test-excludes"]) {
         testExcludes = entryConfig["test-excludes"];
       }
-      const files = await globPromise(path.join(p, "**/*.js"), { ignore: ignoreDirPatterns });
+      const files = await globPromise(path.join(p, "**/*.js"), { ignore: ignoreDirPatterns, follow: true });
       return Promise.all(
         files
           // TODO: load deps.js path from config
