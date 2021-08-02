b0e5e76333f858f34c6237f7fcf09cd5d4775df1
sosukesuzuki <aosukeke@gmail.com>
2021-08-02 15:13:43 +0900

    Use strict mode assert in tests

--- test/xunit-reporter.ts
+++ test/xunit-reporter.ts
@@ -1,4 +1,4 @@
-import assert = require("assert");
+import { strict as assert } from "assert";
 import { oneLineTrim } from "common-tags";
 import { existsSync, promises as fs } from "fs";
 import path from "path";
