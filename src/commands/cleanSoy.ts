5159c63a980a365b42d1aa80c085f7313375cb2d
Teppei Sato <teppeis@gmail.com>
2022-08-06 21:22:07 +0900

    feat(soy): support soyOptions.outputDirectory (#753)

--- src/commands/cleanSoy.ts
+++ src/commands/cleanSoy.ts
@@ -1,18 +1,24 @@
 import rimraf from "rimraf";
 import util from "util";
+import { assertString } from "../assert";
 import { DuckConfig } from "../duckconfig";
 import { logger } from "../logger";
+import { resolveOutputPathFormat } from "../soy";
 
 export type CleanSoyConfig = Required<Pick<DuckConfig, "soyOptions">>;
 
 export async function cleanSoy(config: CleanSoyConfig): Promise<string> {
-  const { outputPathFormat } = config.soyOptions;
-  const outputPath = outputPathFormat
-    .replace("{INPUT_DIRECTORY}", "/**/")
-    .replace("{INPUT_FILE_NAME}", "*")
-    .replace("{INPUT_FILE_NAME_NO_EXT}", "*")
-    .replace("{LOCALE}", "*")
-    .replace("{LOCALE_LOWER_CASE}", "*");
+  const { outputPathFormat, outputDirectory } = config.soyOptions;
+  let outputPath = outputDirectory;
+  if (!outputPath && outputPathFormat) {
+    outputPath = resolveOutputPathFormat(assertString(outputPathFormat), {
+      inputDirectory: "/**/",
+      inputFileName: "*",
+      inputFileNameNoExt: "*",
+    });
+  } else {
+    throw new TypeError("Must set either outputPathFormat or outputDirectory");
+  }
   logger.info(`rm ${outputPath}`);
   await util.promisify(rimraf)(outputPath);
   return outputPath;
--- test/soy.ts
+++ test/soy.ts
@@ -1,5 +1,5 @@
 import { strict as assert } from "assert";
-import { toSoyArgs } from "../src/soy";
+import { calcOutputPath, toSoyArgs } from "../src/soy";
 
 describe("soy", () => {
   describe("toSoyArgs", () => {
@@ -29,6 +29,50 @@ describe("soy", () => {
         "--srcs",
         "/js/foo.soy,/js/bar.soy",
       ]);
+    });
+  });
+  describe("calcOutputPath", () => {
+    it("throws an error when srcs is relative", () => {
+      assert.throws(() => {
+        calcOutputPath("src/js/file.soy", {});
+      }, /absolute/i);
+    });
+    describe("outputDirectory", () => {
+      it("ignores outputDirectory when inputRoots is not specified", () => {
+        const actual = calcOutputPath("/src/js/file.soy", {
+          outputDirectory: "/src/js/soy",
+        });
+        assert.equal(actual, "/src/js/file.soy.js");
+      });
+      it("joins outputDirectory and a relative path from inputRoots to srcs", () => {
+        const actual = calcOutputPath("/src/js/path/to/file.soy", {
+          outputDirectory: "/src/js/soy",
+          inputRoots: ["/src/js"],
+        });
+        assert.equal(actual, "/src/js/soy/path/to/file.soy.js");
+      });
+      it("ignores an inputRoot when srcs doesn't start with it", () => {
+        const actual = calcOutputPath("/src/js/path/to/file.soy", {
+          outputDirectory: "/src/js/soy",
+          inputRoots: ["/hoge", "/fuga"],
+        });
+        assert.equal(actual, "/src/js/path/to/file.soy.js");
+      });
+    });
+    describe("outputPathFormat", () => {
+      it("replaces `INPUT_DIRECTORY` and `INPUT_DIRECTORY`", () => {
+        const actual = calcOutputPath("/aaa/bbb/ccc/ddd/file.name.soy", {
+          outputPathFormat: "{INPUT_DIRECTORY}{INPUT_FILE_NAME}.js",
+        });
+        assert.equal(actual, "/aaa/bbb/ccc/ddd/file.name.soy.js");
+      });
+      it("replaces `INPUT_FILE_NAME_NO_EXT`", () => {
+        const actual = calcOutputPath("/aaa/bbb/ccc/ddd/file.name.soy", {
+          outputPathFormat:
+            "{INPUT_DIRECTORY}yyy/zzz/{INPUT_FILE_NAME_NO_EXT}.js",
+        });
+        assert.equal(actual, "/aaa/bbb/ccc/ddd/yyy/zzz/file.name.js");
+      });
     });
   });
 });
