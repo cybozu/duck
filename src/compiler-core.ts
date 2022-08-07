8a5f6195a53b436fa713726b087b0063b1321c77
teppeis <teppeis@gmail.com>
2022-08-07 23:22:06 +0900

    fix(types): fix types/google-closure-compiler

--- src/compiler-core.ts
+++ src/compiler-core.ts
@@ -4,7 +4,7 @@
  */
 
 import fs from "fs";
-import { compiler as ClosureCompiler } from "google-closure-compiler";
+import closureCompiler from "google-closure-compiler";
 import { dirname } from "path";
 import * as tempy from "tempy";
 import type { DuckConfig } from "./duckconfig.js";
@@ -13,6 +13,7 @@ import { logger } from "./logger.js";
 import type { CompileErrorItem } from "./report.js";
 
 declare const __non_webpack_require__: NodeRequire;
+const ClosureCompiler = closureCompiler.compiler;
 
 export interface CompilerOptions {
   [idx: string]: any;
--- types/google-closure-compiler/index.d.ts
+++ types/google-closure-compiler/index.d.ts
@@ -5,14 +5,18 @@ import type { ChildProcess } from "child_process";
  * https://github.com/google/closure-compiler-npm
  */
 
-export class compiler {
-  public JAR_PATH: string | null;
-  public javaPath: string;
-  constructor(
-    args: { [idx: string]: string } | string[],
-    extraCommandArgs?: string[]
-  );
-  run(
-    callback?: (exitCode: number, stdout: string, stderr: string) => void
-  ): ChildProcess;
+namespace GoogleClosureCompiler {
+  class compiler {
+    public JAR_PATH: string | null;
+    public javaPath: string;
+    constructor(
+      args: { [idx: string]: string } | string[],
+      extraCommandArgs?: string[]
+    );
+    run(
+      callback?: (exitCode: number, stdout: string, stderr: string) => void
+    ): ChildProcess;
+  }
 }
+
+export default GoogleClosureCompiler;
